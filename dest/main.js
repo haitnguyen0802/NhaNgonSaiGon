a69e90511976e2952c575c424f0b9f42a340ec8a
thai <hongthai2007.hongthai2007@gmail.com>
2022-06-27 18:01:28 +0700

    third commit 27/06/2022 Done to appartments

--- dest/main.js
+++ dest/main.js
@@ -1,1 +1,8 @@
-console.log('Hello CFD Circle');
+//Btn Menu Header
+let menuBtn = document.querySelector(".header .header__btnmenu") 
+let mainMenu = document.querySelector(".header .container .nav")
+console.log(mainMenu)
+menuBtn.addEventListener("click",function(element){
+    menuBtn.classList.toggle("active");
+    mainMenu.classList.toggle("active");
+})
