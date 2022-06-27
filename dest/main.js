bd1b0f51c8556f6284cc06c9f734f7ce3cc5139d
thai <hongthai2007.hongthai2007@gmail.com>
2022-06-27 20:02:39 +0700

    27/06/2022 update section hiw | done Section HIW | Done Responsive | Done JS

--- dest/main.js
+++ dest/main.js
@@ -1,8 +1,23 @@
 //Btn Menu Header
 let menuBtn = document.querySelector(".header .header__btnmenu") 
 let mainMenu = document.querySelector(".header .container .nav")
-console.log(mainMenu)
+
 menuBtn.addEventListener("click",function(element){
     menuBtn.classList.toggle("active");
     mainMenu.classList.toggle("active");
+})
+
+//Hiw Selected
+let itemHiw = document.querySelectorAll(".hiw .hiw__items .hiw__items-block")
+currentSelected = 0;
+itemHiw.forEach(function(element,index){
+    if(element.classList.contains("active")){
+        currentSelected = index;
+    }
+    element.addEventListener("click",function(event){
+        itemHiw[currentSelected].classList.remove("active");
+        itemHiw[index].classList.add("active");
+
+        currentSelected = index;
+    })
 })
