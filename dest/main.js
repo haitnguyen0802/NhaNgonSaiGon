3bb180ba35c9249b782cdf1787c11a3e15d6e076
thai <hongthai2007.hongthai2007@gmail.com>
2022-07-05 17:46:57 +0700

    05/07/2022 |  Update Animation | HaitNguyen

--- dest/main.js
+++ dest/main.js
@@ -1,23 +1,46 @@
+//Height Header
+let heightHeader = document.querySelector(".header");
+const mainWrapper = document.querySelector(".mainwrapper");
+window.addEventListener("scroll", function (event) {
+  console.log(this.window.pageYOffset);
+  if (this.window.pageYOffset >= 200) {
+    mainWrapper.classList.add("active");
+    heightHeader.classList.add("active");
+  } else {
+    mainWrapper.classList.remove("active");
+    heightHeader.classList.remove("active", "ani_BottomtoTop");
+  }
+  if (this.window.pageYOffset >= 600) {
+    heightHeader.classList.add("active", "ani_ToptoBottom");
+  } else {
+    heightHeader.classList.replace("ani_ToptoBottom", "ani_BottomtoTop");
+  }
+  if (this.window.pageYOffset > 200 && this.window.pageYOffset < 400) {
+    if (heightHeader.classList.contains("ani_BottomtoTop")) {
+      heightHeader.classList.remove("ani_BottomtoTop");
+    }
+  }
+});
 //Btn Menu Header
-let menuBtn = document.querySelector(".header .header__btnmenu") 
-let mainMenu = document.querySelector(".header .container .nav")
+let menuBtn = document.querySelector(".header .header__btnmenu");
+let mainMenu = document.querySelector(".header .container .nav");
 
-menuBtn.addEventListener("click",function(element){
-    menuBtn.classList.toggle("active");
-    mainMenu.classList.toggle("active");
-})
+menuBtn.addEventListener("click", function (element) {
+  menuBtn.classList.toggle("active");
+  mainMenu.classList.toggle("active");
+});
 
 //Hiw Selected
-let itemHiw = document.querySelectorAll(".hiw .hiw__items .hiw__items-block")
+let itemHiw = document.querySelectorAll(".hiw .hiw__items .hiw__items-block");
 currentSelected = 0;
-itemHiw.forEach(function(element,index){
-    if(element.classList.contains("active")){
-        currentSelected = index;
-    }
-    element.addEventListener("click",function(event){
-        itemHiw[currentSelected].classList.remove("active");
-        itemHiw[index].classList.add("active");
+itemHiw.forEach(function (element, index) {
+  if (element.classList.contains("active")) {
+    currentSelected = index;
+  }
+  element.addEventListener("click", function (event) {
+    itemHiw[currentSelected].classList.remove("active");
+    itemHiw[index].classList.add("active");
 
-        currentSelected = index;
-    })
-})
+    currentSelected = index;
+  });
+});
